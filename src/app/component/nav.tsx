a67c810261307409cde08a78d636bc01cbebb98d
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-29 00:45:09 +0500

    working

---
+++ src/app/component/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div className="w-full shadow-navShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg bodycolour px-4">
+      
+    </div>
+  )
+}
+
+export default nav
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
-import Hero from './component/Hero'
-import Contact from './component/Contact'
-import Project from './component/Project'
-import About from './component/About'
-import Services from './component/services'
+
+import nav from './component/nav'
+//import Hero from './component/Hero'
+//import Contact from './component/Contact'
+//import Project from './component/Project'
+//import About from './component/About'
+//import Services from './component/services'
 
 export default function Home() {
   return (
     <div>
-         <Hero />
+       <nav />
+
+       {/*  <Hero />
          <About />
          <Services />
          <Project />
-         <Contact /> 
+         <Contact />       */}
     </div>
    
   )
