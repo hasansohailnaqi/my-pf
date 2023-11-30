d39262cfd740815a024d2264d47bf561ff17cb41
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-30 21:03:28 +0500

    working

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -7,38 +7,33 @@ import { IoCloudDownloadOutline } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
-    <header className='flex justify-between items-center bg-slate-500'>
-    <a className=" ">
-      <Image src={require("../../../public/logo/MSC3.0 LOGO.png")} alt="mSC3.0" 
-      width={100} 
-      height={80}
-      />
-      <a href="/cv/myCv.pdf" download="your-cv-file-name.pdf"> 
-      <button className="dou items-center bg-gray-100 border-0 py-1 px-3 hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-      >Doc 
-        <IoCloudDownloadOutline className="text-xl ml-2" />
-        </button>
-        </a>
-    </a>
-    <ul>
-      <li>
-        <a href="#">Home</a>
-      </li>
-      <li>
-        <a href="#">About Us</a>
-      </li>
-      <li>
-        <a href="#">Pricing</a>
-      </li>
-      <li>
-        <a href="#">Products</a>
-      </li>
-    </ul>
-    <span className="text-right">
-      <a href="#" className="btn">Join us</a>
-    </span>
-  </header>
-  
+    <header className=" items-center bg-slate-700">
+  <nav className="bg-blue-500 p-4 ">
+    <div className="flex justify-between">
+  <a href="#">
+    <img className=" inline h-7" src="https://res.cloudinary.com/thirus/image/upload/v1628614672/logos/circleai_dm9slt.png" alt="" />
+  </a>
+  <ul className='flex bg-slate-500'>
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">About Us</a>
+    </li>
+    <li>
+      <a href="#">Pricing</a>
+    </li>
+    <li>
+      <a href="#">Products</a>
+    </li>
+  </ul>
+  <span className="text-right">
+    <a href="#" className="btn">Join us</a>
+  </span>
+  </div>
+  </nav>
+</header>
+
   )
 }
 
