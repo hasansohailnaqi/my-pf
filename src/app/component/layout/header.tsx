a87ba283ae2f07adf6ee3cacfabb04a1a66ba866
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2023-11-30 05:08:00 +0500

    workig

--- src/app/component/layout/header.tsx
+++ src/app/component/layout/header.tsx
@@ -3,9 +3,17 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      navbar
+      
+      <ul>
+        <li></li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Blog</li>
+      </ul>
     </div>
   )
 }
+
 
 export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
          <Services />
          <Project />
          <Contact />       
-          <nav />     */}
+               */}
     </div>
    
   )
