77e665ee3828dc1dc655253bce3e6ffcf5ce7e77
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2024-02-03 04:04:54 +0500

    working

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,15 @@ import Image from "next/image"
 import { HiArrowDown } from "react-icons/hi"
 import Link from "next/link"
 import Typewriter from "typewriter-effect"
+
+
 const HeroSection = () => {
   return (
     <section className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src=""
+            src={"/boss.jpg"}
             alt=""
             width={325}
             height={325}
@@ -20,21 +22,23 @@ const HeroSection = () => {
        
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Hasan Sohail Naqi!</h1>
-          <p className="text-lg font-bold mt-4 mb-6 md:text-2xl">
-             <Typewriter
+          <h3 className="text-lg font-bold mt-4 mb-6 md:text-2xl">
+            
+             <Typewriter 
   options={{
     strings: [ "Web Developer", "UI/UX Designer","Real Estate Consultant"],
     autoStart: true,
     loop: true,
   }}
 />
+
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               JamStack Developer{" "}
             </span>
             based in Karachi, Pakistan. Working towards creating software that
             makes life easier and more meaningful.
-          </p>
+          </h3>
           
           <Link href="/project"  
             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
