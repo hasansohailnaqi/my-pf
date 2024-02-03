115a6b0ba48b718769da33705d9bc672c1a6a5dc
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2024-02-04 01:04:20 +0500

    working

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -24,7 +24,7 @@ const Project = () => {
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
+              Msc-Photo-App
             </h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
               Shooting Stars
